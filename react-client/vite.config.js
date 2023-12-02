5b6709e54bfe66e169c816eb6b3f97178272eddd
Kostadin Minov <minovkostadin@gmail.com>
2023-12-02 15:47:13 +0200

    Update project

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import viteBasicSslPlugin from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+  server: {
+    host: 'localhost',
+    port: 5173,
+    https: true,
+  },
   plugins:
     [
       react(),
