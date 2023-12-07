3da4b226bf0ad80f0623d899a07c0ec9a790bfb0
Kostadin Minov <minovkostadin@gmail.com>
2023-12-07 21:57:20 +0200

    fix spinner height

--- react-client/src/components/Spinner.jsx
+++ react-client/src/components/Spinner.jsx
@@ -7,9 +7,9 @@ const Spinner = () =>
     return (
         <div
             role='status'
-            className='flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'
+            className='flex items-end justify-center pt-4 px-4 pb-20 text-center sm:block sm:p-0'
         >
-            <span className='hidden sm:inline-block sm:align-middle sm:h-screen'></span>
+            <span className='hidden sm:inline-block sm:align-middle'></span>
             <FontAwesomeIcon icon={faSpinner} spin size='2xl' style={{ color: '#14b8a6', }} />
             <span className='sr-only'>Loading...</span>
         </div>
